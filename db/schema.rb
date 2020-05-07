52407529b90c54ee2516309e1c2f02d15303d90e
sabawasabi <ON.0723_i@outlook.jp>
2020-05-07 15:24:25 +0900

    user_colum_add

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_05_02_055350) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "gender", default: "", null: false
+    t.string "profile", default: ""
+    t.string "icon", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

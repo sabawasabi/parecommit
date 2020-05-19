88f17827dc3e264e2776bda49b0987e13904a759
sabawasabi <ON.0723_i@outlook.jp>
2020-05-19 11:08:14 +0900

    target_add

---
+++ db/migrate/20200519011851_create_targets.rb
@@ -0,0 +1,12 @@
+class CreateTargets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :targets do |t|
+      t.references :user,        foreign_key:true
+      t.string     :target,      null: false
+      t.string     :explanation, null: false
+      t.string     :limit,       null: false
+      t.string     :status,      null: false
+      t.timestamps
+    end
+  end
+end

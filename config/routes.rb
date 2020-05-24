74b89810aede344c4da1c08f6ab0e4822cf7d0a1
sabawasabi <ON.0723_i@outlook.jp>
2020-05-25 03:52:10 +0900

    add:match機能の追加
    検索から一覧を表示し、表示された目標一覧の中から任意の項目を選択、詳細ページから確定をクリック後、matchテーブルへ情報を格納

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root "targets#index"
-  resources :users, only: [:edit, :update]
+  resources :users,   only: [:edit, :update]
   resources :targets, only: [:index, :new, :create, :show] do
     collection do
       get 'search'
     end
   end
-  resources :matchs, only: [:new, :create]
+  resources :matchs do
+    collection do
+      get 'match_target', to: 'matchs#match_target'
+      get 'done',         to: 'matchs#done'
+    end
+  end
 end
